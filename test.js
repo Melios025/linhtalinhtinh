8fcb47d0613a42d078036f6bea5d759eb2a000bf
Melios025 <thaipham02509@gmail.com>
2026-05-23 02:35:45 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Tool clone
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-27
+// @description  try to take over the world!
+// @author       Melios
+// @match        https://hoathinh3d.co/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+  loadQuizData()
+
+})();
