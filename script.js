8de90185d78f0fc965e76ba06ddddb99a00d3963
Melios025 <thaipham02509@gmail.com>
2026-05-23 03:42:07 +0700

    update version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tool for clone
 // @namespace    http://tampermonkey.net/
-// @version      2.4.3
+// @version      2.4.4
 // @description  Mine use only
 // @author       Melios
 // @match        https://hoathinh3d.co/*
