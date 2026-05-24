5953a39ca05883a57c1e1eb64cdc465693db46ad
Melios025 <thaipham02509@gmail.com>
2026-05-24 20:45:13 +0700

    updated: 2024-06-30T12:00:00Z

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tool for clone
 // @namespace    http://tampermonkey.net/
-// @version      2.5.11
+// @version      2.5.12
 // @description  Tool auto các hoạt động hàng ngày trên hoathinh3d.co, phục vụ mục đích cá nhân
 // @author       Melios
 // @match        https://hoathinh3d.co/*
