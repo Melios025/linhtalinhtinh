f9d5b750dbe146684003f0c1d4711aebbc92e449
Melios025 <thaipham02509@gmail.com>
2026-05-24 20:13:36 +0700

    update version to 2.5.7, add some styles to control panel and buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tool for clone
 // @namespace    http://tampermonkey.net/
-// @version      2.5.6
+// @version      2.5.7
 // @description  Tool auto các hoạt động hàng ngày trên hoathinh3d.co, phục vụ mục đích cá nhân
 // @author       Melios
 // @match        https://hoathinh3d.co/*
